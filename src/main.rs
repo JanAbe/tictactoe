42774493f19d7800f5cf160ee31fa7c59f396a84
Jan Abe de Waal <jandewaal33@gmail.com>
2020-04-07 21:31:26 +0200

    Create generate_win_combinations func

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,61 @@ use std::collections::HashMap;
 fn main() {
     println!("Hello, world!");
 
-    let mut input = String::new();
-    io::stdin().read_line(&mut input)
-        .expect("Failed to read line");
+    // let mut input = String::new();
+    // io::stdin().read_line(&mut input)
+    //     .expect("Failed to read line");
 
-    println!("Your input {}", input);
+    // println!("Your input {}", input);
+
+    let mut x = Vec::new();
+    x.push((0,0));
+    x.push((1,0));
+    x.push((2,0));
+    x.push((3,0));
+    x.push((4,0));
+    let size3_win_combos = win_combinations(3);
+    println!("{:?}", size3_win_combos);
+    /* output of win_combinations(3) =
+        [
+            [(0, 0), (0, 1), (0, 2)], 
+            [(0, 0), (1, 0), (2, 0)], 
+
+            [(1, 0), (1, 1), (1, 2)], 
+            [(0, 1), (1, 1), (2, 1)], 
+
+            [(2, 0), (2, 1), (2, 2)], 
+            [(0, 2), (1, 2), (2, 2)], 
+
+            [(0, 0), (1, 1), (2, 2)], 
+            [(0, 2), (1, 1), (2, 0)]
+        ]
+    */
+
+    let size5_win_combos = win_combinations(5);
+    let won = size5_win_combos.contains(&x);
+    println!("{:?}", size5_win_combos);
+    println!("{}", won);
+    /* output of win_combinations(5) =
+        [
+            [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4)], 
+            [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0)], 
+
+            [(1, 0), (1, 1), (1, 2), (1, 3), (1, 4)], 
+            [(0, 1), (1, 1), (2, 1), (3, 1), (4, 1)], 
+
+            [(2, 0), (2, 1), (2, 2), (2, 3), (2, 4)], 
+            [(0, 2), (1, 2), (2, 2), (3, 2), (4, 2)], 
+
+            [(3, 0), (3, 1), (3, 2), (3, 3), (3, 4)], 
+            [(0, 3), (1, 3), (2, 3), (3, 3), (4, 3)], 
+
+            [(4, 0), (4, 1), (4, 2), (4, 3), (4, 4)], 
+            [(0, 4), (1, 4), (2, 4), (3, 4), (4, 4)], 
+
+            [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4)], 
+            [(0, 4), (1, 3), (2, 2), (3, 1), (4, 0)]
+        ]
+    */
 }
 
 
@@ -31,21 +81,23 @@ fn win_combinations(n: u8) -> Vec<Vec<(u8, u8)>> {
     let mut winnable_combinations = Vec::new();
     let mut left_diagonal_combinations = Vec::new();
     for i in 0..n {
-        left_diagonal_combinations.push((i, i));
-        let mut row = Vec::new();
-        let mut column = Vec::new();
+        let mut rows = Vec::new();
+        let mut columns = Vec::new();
         for j in 0..n {
-            column.push((j, i));
-            row.push((i, j))
+            rows.push((i, j));
+            columns.push((j, i));
         }
-        winnable_combinations.push(row);
-        winnable_combinations.push(column);
+        
+        left_diagonal_combinations.push((i, i));
+        winnable_combinations.push(rows);
+        winnable_combinations.push(columns);
     }
 
     winnable_combinations.push(left_diagonal_combinations);
 
+    // see if this can be executed/placed in the for loop above.
     let mut right_diagonal_combinations = Vec::new();
-    for i in 1..n-1 {
+    for i in 0..n {
         let j = (n-1)-i;
         right_diagonal_combinations.push((i, j));
     }
