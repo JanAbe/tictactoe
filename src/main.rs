eb1dfd97efe0b10d9c235f600b9174253437b8a0
Jan Abe de Waal <jandewaal33@gmail.com>
2020-04-12 16:12:02 +0200

    Further work on tic tac toe

--- src/main.rs
+++ src/main.rs
@@ -21,35 +21,16 @@ fn main() {
         length: board_length
     };
 
-    board.cells.insert((0,0), PlayerValue::O);
     println!("\n");
     board.draw();
 
-    let game = Game {
+    let mut game = Game {
         is_over: false,
-        turn_counter: 0,
+        turn_counter: 1,
         board: board,
     };
 
-    // let mut game_over = false;
-    // let mut turn = 1;
-    // let mut board = Board{
-    //     cells: HashMap::new(), 
-    //     length: board_length
-    // };
-
-    // while !game_over {
-    //     if turn % 2 == 0 {
-    //         prompt_player(&mut board, PlayerValue::X);
-    //     } else {
-    //         prompt_player(&mut board, PlayerValue::O);
-    //     }
-
-    //     turn += 1;
-    //     if turn == 10 {
-    //         game_over = true;
-    //     }
-    // }
+    game.play();
 }
 
 
@@ -65,18 +46,107 @@ impl Game{
         while !self.is_over {
             self.prompt_player();
         }
+
+        // why does this do the reversed of what i expect?
+        // maybe the turn counter doesn't get increased before i check if game won or something, idk :c
+        if self.turn_counter % 2 == 0 {
+            println!("Game over, player O has won!");
+        } else {
+            println!("Game over, player X has won!");
+        }
     }
 
-    /// prompt_player prompts a player to input his new move and
-    /// updates the board increases the turn counter
+    // check_player_won checks if a player has won by looking at all possible win combinations.
+    // for each win combination it looks which player has chosen the tile, and if all tiles within the
+    // winnable combination are chosen by teh same player.
+    fn check_player_won(&self) -> bool {
+        // need to store this in game/board field. It is unecessary to generate all winnable combination each time this function is called,
+        // as it doesn't change.
+        let win_combinations = self.board.generate_win_combinations();
+        let mut player_won = true;
+        // todo: atm is only works for a board of size 3
+        // need to make this size independent!!!
+        for i in 0..win_combinations.len() {
+        //     for j in 0..win_combinations[i].len()-1 {
+        //         player_won = match self.board.cells.get(&win_combinations[i][j]) {
+        //             Some(current_tile) =>
+        //                 match self.board.cells.get(&win_combinations[i][j+1]) {
+        //                     Some(next_tile) => (current_tile == next_tile) && player_won,
+        //                     _ => false
+        //                 }
+        //             _ => false
+        //         };
+
+        //     }
+        //     if player_won {
+        //         break;
+        //     }
+
+            player_won = match self.board.cells.get(&win_combinations[i][0]) {
+                Some(first_tile) => 
+                    match self.board.cells.get(&win_combinations[i][1]) {
+                        Some(second_tile) => 
+                            match self.board.cells.get(&win_combinations[i][2]) {
+                                Some(third_tile) => (first_tile == second_tile) && (first_tile == third_tile),
+                                _ => false
+                            }
+                        _ => false
+                    }
+                _ => false
+            };
+
+            if player_won {
+                break;
+            }
+        }
+
+        return player_won;
+    }
+
+    /// prompt_player prompts a player to input his new move, 
+    /// updates the board and increases the turn counter
     fn prompt_player(&mut self) {
         println!("Enter tile coordinates, like: x,y");
         let mut tile_coords = String::new();
         io::stdin().read_line(&mut tile_coords)
             .expect("Failed to read tile coordinates");
         
-        let tile_coords: Vec<&str> = tile_coords.split(',').collect();
-        println!("{:?}", tile_coords);
+        let tile_coords: Vec<&str> = tile_coords.trim().split(',').collect();
+        if tile_coords.len() != 2 {
+            self.prompt_player();
+        }
+
+        // dit in een loop stoppen, dan bij Err(_) => continue schrijven ?
+        // maar dan heb ik wel een loop in een loop, omdat deze functie wordt aangeroepen in de game loop.
+        // Weet niet of dat erg is.
+        let x: u8 = match tile_coords[0].parse() {
+            Ok(num) => num,
+            Err(_) => 0 //self.prompt_player(), // if parsing fails -> reprompt the user
+        };
+
+        let y: u8 = match tile_coords[1].parse() {
+            Ok(num) => num,
+            Err(_) => 0 //self.prompt_player(),
+        };
+
+        let n = self.board.length;
+        if x > n || y > n {
+            println!("Provided coords are outside of board space. Choose again:");
+            self.prompt_player();
+        }
+
+        let tile_is_taken = self.board.cells.contains_key(&(x,y));
+        if tile_is_taken {
+            println!("Tile is already chosen. Chose another tile.");
+            self.prompt_player();
+        }
+
+        if self.turn_counter % 2 == 0 {
+            self.board.cells.insert((x,y), PlayerValue::X);
+        } else {
+            self.board.cells.insert((x,y), PlayerValue::O);
+        }
+
         // can maybe make this function recursive? 
         // todo: parse tile_coords to u8's
         // if no number chosen, or if number is outside of board size -> ask for user input again
@@ -91,6 +161,9 @@ impl Game{
         // after a succesful turn -> draw board to convey new state
 
         self.turn_counter += 1; // does turn_counter need to be mut?
+        println!("");
+        self.board.draw();
+        self.is_over = self.check_player_won();
     }
 }
 
@@ -132,11 +205,27 @@ impl Board {
         return winnable_combinations;
     }
 
-    /// draw draws the board on standard output
+    /// draw draws the board and displays it on standard output
     fn draw(&self) {
-        let mut output: Vec<String> = Vec::new();
-        // let two_spaces: String = String::from("  ");
+        /*
+            // todo: improve display of first row
+            // atm each cell has a width of 3
+            // and for the first row in the middle, like _i_, the column number is placed.
+            // Problem: it doesn't work when the column_number becomes longer than 1 digit
+            // e.g. 12 or 44. It screws up the mark up of the first row.
+            // so need to build something that checks if the board_length is 1 digit, 2 digits or 3 digits long
+            // (can use the fact that the board length can never be longer than 255 (so need to support 3 digits max))
+            // let w: usize;
+            // if n < 10 {
+            //     w = 3; // -5-
+            // } else if n > 99 {
+            //     w = 5; // -244-
+            // } else {
+            //     w = 4; // -23-
+            // }
+        */
 
+        let mut output: Vec<String> = Vec::new();
         output.push(String::from("  "));
 
         let n = self.length;
